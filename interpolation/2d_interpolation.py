dce82d7974abfe5fcf9380fa3aa1e3ca6d742e89
Adam Mazur <adam.mazur03@icloud.com>
2023-12-11 20:07:06 +0100

    added image interpolation

--- interpolation/2d_interpolation.py
+++ interpolation/2d_interpolation.py
@@ -1,5 +1,6 @@
 import numpy as np
 import cv2
+from sklearn import metrics
 
 grey_image = cv2.imread("interpolation/image.jpg", cv2.IMREAD_GRAYSCALE)
 
@@ -31,13 +32,46 @@ def max_pooling(image, kernel, stride):
 
     return result
 
+# Kernels from 1d interpolation
+def linear_kernel2d(x, offset, width):
+    x = x - offset
+    x = x / width
+    return (1 - np.abs(x)) * (np.abs(x) < 1)
+
+def sinc_kernel2d(x, offset, width, alpha = np.inf):
+    x = x - offset
+    x = x / width
+    return (x >= -alpha) * (x < alpha) * np.sinc(x)
+
+# Function from 1d interpolation
+def conv1d_interpolate(x_measure, y_measure, x_interpolate, kernel: callable):
+    width = x_measure[1] - x_measure[0]
+    kernels = np.asarray([kernel(x_interpolate, offset=offset, width=width) for offset in x_measure])
+    return y_measure @ kernels
+
+# Function to interpolate image
+def image_interpolate(image, kernel: callable, ratio):
+
+    def row_column_interpolate(row):
+        x_measure = np.arange(len(row))
+        x_interpolate = np.linspace(0, len(row), ratio * len(row), endpoint=False)
+        return conv1d_interpolate(x_measure, row, x_interpolate, kernel)
+
+    interpolated = np.apply_along_axis(row_column_interpolate, 1, image)
+    return np.apply_along_axis(row_column_interpolate, 0, interpolated)
 
 # Number of pixels to step by, size of imput image must equal mod stride = 0
 stride = 2
+ratio = 2
 
 average_pooled_image = average_pooling(grey_image, custom_kernel, stride)
 max_pooled_image = max_pooling(grey_image, custom_kernel, stride)
+interpolated_image = image_interpolate(average_pooled_image, linear_kernel2d, ratio)
 
 # Save rusults
 cv2.imwrite("interpolation/resultavg.jpg", average_pooled_image)
 cv2.imwrite("interpolation/resultmax.jpg", max_pooled_image)
+cv2.imwrite("interpolation/interpolated_image.jpg", interpolated_image)
+
+# Print Metrics mean squared error
+print(f"MSE: {metrics.mean_squared_error(grey_image, interpolated_image):.8f}")
